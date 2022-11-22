026bd18344b854ae88b31d2405ca98ac853c10a6
bettyfeyissa <bettyfeyissa@gmail.com>
2022-11-21 21:56:02 -0600

    intial commit

---
+++ src/test/java/org/dream/firstAPI/controllers/ApiControllers.java
@@ -0,0 +1,11 @@
+package org.dream.firstAPI.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiControllers {
+    public String getPage;
+//@GetMapping(value = "/")
+    //return "Hello from out API"
+}
