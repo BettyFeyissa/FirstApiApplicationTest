6a56a30fa042aba2f1c2f1f51f57036cc61944a8
bettyfeyissa <bettyfeyissa@gmail.com>
2022-11-21 22:38:34 -0600

    creadeted a new package and user class

---
+++ src/test/java/org/dream/firstAPI/Models/User.java
@@ -0,0 +1,9 @@
+package org.dream.firstAPI.Models;
+
+public class User {
+    private long id;
+    private String firstName;
+    private  String lastName;
+    private int age;
+    private String occupation;
+}
